c160d38c3a4af84d4271e4ed022473205c9c379d
anduarte3 <andreqt8@gmail.com>
2025-07-30 12:51:20 +0100

    initial commit

---
+++ server/src/test/java/com/eqlyn/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eqlyn.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
